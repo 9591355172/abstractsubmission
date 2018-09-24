af826489b607aa0f44618710fb4572b878b61520
9591355172 <abhijeet428001@gmail.com>
2018-09-24 15:49:19 +0530

    update styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
        def after_sign_in_path_for(resource_or_scope)
@@ -13,4 +14,11 @@ class ApplicationController < ActionController::Base
 
         end
      end
+  
+
+  protected
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile_number, :organization, :designation, :webiste_url, :promotion])
+    end
+
 end
