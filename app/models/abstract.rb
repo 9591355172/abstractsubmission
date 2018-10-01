540e75df2ee4581a815327cb7cd4152cc74339b4
9591355172 <abhijeet428001@gmail.com>
2018-10-01 12:57:58 +0530

    social share button

--- app/models/abstract.rb
+++ app/models/abstract.rb
@@ -2,9 +2,9 @@ class Abstract < ApplicationRecord
 	belongs_to :user, optional: true
 	validates_presence_of :abstract_url, :user_id
 
-	after_create :abstract_send
+	# after_create :abstract_send
 
-	def abstract_send
-		AbstractMailer.abstract_send(self).deliver
-	end
+	# def abstract_send
+	# 	AbstractMailer.abstract_send(self).deliver
+	# end
 end
