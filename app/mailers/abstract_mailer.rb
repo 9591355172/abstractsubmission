cd249097a4b9ba266573b2c018d811d12792715c
9591355172 <abhijeet428001@gmail.com>
2018-10-04 14:35:58 +0530

    category of abstract

--- app/mailers/abstract_mailer.rb
+++ app/mailers/abstract_mailer.rb
@@ -1,9 +1,9 @@
 class AbstractMailer < ApplicationMailer
-default from: "abhijeet428001@gmail.com"
+# default from: "abhijeet428001@gmail.com"
 
-	def abstract_send(abstract)
-		@abstract = abstract
-		mail(to: "abhijeet428001@gmail.com", subject: 'Sample Email')
+# 	def abstract_send(abstract)
+# 		@abstract = abstract
+# 		mail(to: "abhijeet428001@gmail.com", subject: 'Sample Email')
 
-	end
+# 	end
 end
