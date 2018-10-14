95356e3e2b87d012bee42dfb95cde19b0dc8b31e
9591355172 <abhijeet428001@gmail.com>
2018-10-14 16:46:37 +0530

    track list

---
+++ app/controllers/track_detail_controller.rb
@@ -0,0 +1,20 @@
+class TrackDetailController < ApplicationController
+	def designthinking
+	end
+	def engaging
+	end
+	def innovating
+	end
+	def extremeevents
+	end
+	def inequality
+	end
+	def management
+	end
+	def social
+	end
+	def sustainability
+	end
+	def sustainbusiness
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # resources :payments
   resources :abstracts
+  resources :track_detail
   get 'abstract' => 'home#abstract'
   get 'about' => 'home#about'
   get 'fees' => 'home#fees'
@@ -30,6 +31,15 @@ Rails.application.routes.draw do
   get 'sponsorship' => 'home#sponsors'
   get 'schedule' => 'home#schedule'
   get 'hotels' => 'home#hotels'
+  get '/tracks/designthinking' => 'track_detail#designthinking'
+  get '/tracks/engaging' => 'track_detail#engaging'
+  get '/tracks/innovating' => 'track_detail#innovating'
+  get '/tracks/extremeevents' => 'track_detail#extremeevents'
+  get '/tracks/inequality' => 'track_detail#inequality'
+  get '/tracks/management' => 'track_detail#management'
+  get '/tracks/social' => 'track_detail#social'
+  get '/tracks/sustainability' => 'track_detail#sustainability'
+  get '/tracks/sustainbusiness' => 'track_detail#sustainbusiness'
   post 'payments' => 'home#payment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
